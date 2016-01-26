c1835d6f4834ba1af75befb849e9532c32bc6ade
Kseol <krendec@gmail.com>
2016-01-26 10:34:58 +0300

    added_theme

---
+++ emac_work_ver-theme.el
@@ -0,0 +1,9 @@
+(deftheme emac_work_ver
+  "Created 2016-01-26.")
+
+(custom-theme-set-variables
+ 'emac_work_ver
+ '(custom-safe-themes (quote ("85b426d9421ec5bdb5459bfe8bab32ee5ecfeed8728a1aa78e9730e947bf79b5" default)))
+ '(pdf-view-midnight-colors (quote ("light blue" . "gray7"))))
+
+(provide-theme 'emac_work_ver)
